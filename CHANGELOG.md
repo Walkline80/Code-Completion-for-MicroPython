02365f73548695f0d3d691d4ca94e511bac546bc
Walkline Wang <walkline@163.com>
2024-06-16 14:19:07 +0800

    初始化项目
    * 增加 .gitignore 文件
    * 增加 vscode extension 脚手架文件
    * 删除无用文件
    * 增加 操作流程 文档
    
    Signed-off-by: Walkline Wang <walkline@163.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Code Completion for MicroPython Change Log
+
+## [Unreleased]
+
+## [Version 0.0.1]: 2024-06-16
+
+### Added
+
+初始化项目
+
+* 增加 .gitignore 文件
+* 增加 vscode extension 脚手架文件
+* 增加 操作流程 文档
+
+### Removed
+
+* 删除无用文件
+
+[unreleased]: https://gitee.com/walkline/code-completion-for-micropython/compare/v0.0.1...HEAD
+[Version 0.0.1]: https://gitee.com/walkline/code-completion-for-micropython/releases/tag/v0.0.1
---
+++ src/extension.ts
@@ -0,0 +1,57 @@
+/* Copyright © 2024 Walkline Wang (https://walkline.wang)
+ * Gitee: https://gitee.com/walkline/code-completion-for-micropython
+ */
+import * as vscode from 'vscode';
+
+let ext = vscode.extensions.getExtension("walklinewang.code-completion-for-micropython");
+
+const config_name = "python";
+const key_1 = "autoComplete.extraPaths";
+const key_2 = "analysis.extraPaths";
+const value = [`${ext?.extensionPath}\\code_completion`];
+
+// https://blog.csdn.net/forward_huan/article/details/108084802
+function update_configuration(cfgName: string, key: string, value: any,
+	configurationTarget: vscode.ConfigurationTarget | boolean) {
+	return new Promise((resolve, reject) => {
+		let cfg = vscode.workspace.getConfiguration(cfgName);
+
+		if (cfg.has(key)) {
+			cfg.update(key, value, configurationTarget)
+				.then(() => {
+					vscode.window.showInformationMessage('Code completion setup successed');
+					resolve(true);
+				}).then(undefined, err => {
+					vscode.window.showErrorMessage('Code completion setup failed');
+					reject(err);
+				});
+		} else {
+			resolve(false);
+		}
+	});
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('extension.setup', () => {
+		if (vscode.workspace.workspaceFolders?.length !== 0) {
+			update_configuration(config_name, key_1, value, vscode.ConfigurationTarget.Workspace);
+			update_configuration(config_name, key_2, value, vscode.ConfigurationTarget.Workspace);
+
+			// vscode.workspace.workspaceFolders?.forEach(folder => {
+			// 	const workspace_root = folder.uri.fsPath;
+			// 	const abconfig = vscode.Uri.file(`${workspace_root}\\abconfig`);
+
+			// 	vscode.workspace.fs.stat(abconfig).then(
+			// 		(onfulfilled) =>{
+			// 			update_configuration(config_name, key_1, value, vscode.ConfigurationTarget.Workspace);
+			// 			update_configuration(config_name, key_2, value, vscode.ConfigurationTarget.Workspace);
+			// 		}
+			// 	);
+			// });
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
