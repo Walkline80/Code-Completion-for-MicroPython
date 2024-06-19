d019128abdc724fd984a800df6f817121f752fb7
Walkline Wang <walkline@163.com>
2024-06-19 23:06:21 +0800

    extension: 优化 `update_configuration()` 函数执行顺序，并增加错误捕捉。
    优化顺序，捕捉错误是没错，但是代码好丑啊。。。。
    
    Signed-off-by: Walkline Wang <walkline@163.com>

--- src/extension.ts
+++ src/extension.ts
@@ -20,11 +20,10 @@ function update_configuration(cfgName: string, key: string, value: any,
 		if (cfg.has(key)) {
 			cfg.update(key, value, configurationTarget)
 				.then(() => {
-					vscode.window.showInformationMessage('Update settings successed');
 					resolve(true);
-				}).then(undefined, err => {
-					vscode.window.showErrorMessage(`Update settings failed: ${err.message}`, 'OK');
-					reject(err);
+				})
+				.then(undefined, error => {
+					reject(error);
 				});
 		} else {resolve(false);}
 	});
@@ -32,19 +31,45 @@ function update_configuration(cfgName: string, key: string, value: any,
 
 export function activate(context: vscode.ExtensionContext) {
 	const enable = vscode.commands.registerCommand('extension.enable', () => {
-		if (vscode.workspace.workspaceFolders?.length !== 0) {
-			update_configuration(config_name, key_1, [value], vscode.ConfigurationTarget.Workspace);
-			update_configuration(config_name, key_2, [value], vscode.ConfigurationTarget.Workspace);
-			update_configuration(config_name, key_3, value, vscode.ConfigurationTarget.Workspace);
-		}
+		update_configuration(config_name, key_1, [value], vscode.ConfigurationTarget.Workspace)
+		.then((resolved) => {
+			if (resolved) {
+				update_configuration(config_name, key_2, [value], vscode.ConfigurationTarget.Workspace)
+				.then((resolved) => {
+					if (resolved) {
+						update_configuration(config_name, key_3, value, vscode.ConfigurationTarget.Workspace)
+						.then(() => {
+							vscode.window.showInformationMessage('Update settings successed.');
+						});
+					}
+				});
+			} else {
+				vscode.window.showErrorMessage(`Update settings failed: Configuration key '${config_name}' not found.`, 'OK');
+			}
+		}).catch((error) => {
+			vscode.window.showErrorMessage(`Update settings failed: ${error.message}`, 'OK');
+		});
 	});
 
 	const disable = vscode.commands.registerCommand('extension.disable', () => {
-		if (vscode.workspace.workspaceFolders?.length !== 0) {
-			update_configuration(config_name, key_1, [], vscode.ConfigurationTarget.Workspace);
-			update_configuration(config_name, key_2, [], vscode.ConfigurationTarget.Workspace);
-			update_configuration(config_name, key_3, "", vscode.ConfigurationTarget.Workspace);
-		}
+		update_configuration(config_name, key_1, [], vscode.ConfigurationTarget.Workspace)
+		.then((resolved) => {
+			if (resolved) {
+				update_configuration(config_name, key_2, [], vscode.ConfigurationTarget.Workspace)
+				.then((resolved) => {
+					if (resolved) {
+						update_configuration(config_name, key_3, "", vscode.ConfigurationTarget.Workspace)
+						.then(() => {
+							vscode.window.showInformationMessage('Update settings successed.');
+						});
+					}
+				});
+			} else {
+				vscode.window.showErrorMessage(`Update settings failed: Configuration '${config_name}' not found.`, 'OK');
+			}
+		}).catch((error) => {
+			vscode.window.showErrorMessage(`Update settings failed: ${error.message}`, 'OK');
+		});
 	});
 
 	context.subscriptions.push(enable, disable);
