f44e1a55c8930520be88f481d505855ece91f921
Walkline Wang <walkline@163.com>
2024-06-19 16:41:10 +0800

    增加代码补全类库文件
    * asyncio.py
    * re.py
    * select.py
    * socket.py
    * uasyncio.py
    
    Signed-off-by: Walkline Wang <walkline@163.com>

---
+++ code_completion/re.py
@@ -0,0 +1,134 @@
+'''
+simple regular expressions
+
+This module implements a subset of the corresponding CPython module, as described below.
+
+For more information, refer to the original CPython documentation: re.
+
+This module implements regular expression operations.
+
+Regular expression syntax supported is a subset of CPython re module (and actually is a subset of POSIX extended regular expressions).
+
+[View Doc](https://docs.micropython.org/en/latest/library/re.html)
+'''
+# Constants
+DEBUG = ... # Flag value, display debug information about compiled expression.
+
+
+class Regex(object):
+	'''
+	Compiled regular expression.
+
+	Instances of this class are created using `re.compile()`.
+	'''
+	def match(self, string: str):
+		'''
+		Similar to the module-level function `match()`.
+
+		Using methods is (much) more efficient if the same regex is applied to multiple strings.
+		'''
+
+	def search(self, string: str):
+		'''
+		Similar to the module-level function `search()`.
+
+		Using methods is (much) more efficient if the same regex is applied to multiple strings.
+		'''
+
+	def sub(self, replace: str, string: str, count: int = 0, flags: int = 0, /):
+		'''
+		Similar to the module-level function`sub()`.
+
+		Using methods is (much) more efficient if the same regex is applied to multiple strings.
+		'''
+
+	def split(self, string: str, max_split: int = -1, /) -> list:
+		'''
+		Split a `string` using regex.
+
+		If `max_split` is given, it specifies maximum number of splits to perform.
+
+		Returns list of strings (there may be up to max_split+1 elements if it’s specified).
+		'''
+
+
+class Match(object):
+	'''
+	Match objects as returned by `match()` and `search()` methods, and passed to the replacement function in `sub()`.
+	'''
+	def group(self, index: int):
+		'''
+		Return matching (sub)string.
+
+		`index` is 0 for entire match, 1 and above for each capturing group.
+
+		Only numeric groups are supported.
+		'''
+
+	def groups(self) -> tuple:
+		'''
+		Return a tuple containing all the substrings of the groups of the match.
+
+		Note: availability of this method depends on MicroPython port.
+		'''
+
+	def start(self, index=None) -> int:
+		'''
+		Return the index in the original string of the start of the substring group that was matched.
+
+		`index` defaults to the entire group, otherwise it will select a group.
+
+		Note: availability of these methods depends on MicroPython port.
+		'''
+
+	def end(self, index=None) -> int:
+		'''
+		Return the index in the original string of the end of the substring group that was matched.
+
+		`index` defaults to the entire group, otherwise it will select a group.
+
+		Note: availability of these methods depends on MicroPython port.
+		'''
+
+	def span(self, index=None) -> tuple:
+		'''
+		Returns the 2-tuple `(match.start(index), match.end(index))`.
+
+		Note: availability of this method depends on MicroPython port.
+		'''
+
+
+# Functions
+def compile(regex_str, flags=None) -> Regex:
+	'''Compile regular expression, return `regex` object.'''
+
+def match(regex_str: str, string: str) -> Match:
+	'''
+	Compile `regex_str` and match against `string`.
+
+	Match always happens from starting position in a string.
+	'''
+
+def search(regex_str: str, string: str) -> Match:
+	'''
+	Compile `regex_str` and search it in a `string`.
+
+	Unlike `match`, this will search string for first position which matches regex (which still may be 0 if regex is anchored).
+	'''
+
+def sub(regex_str: str, replace: str | function, string: str, count: int = 0, flags: int = 0, /) -> str:
+	'''
+	Compile `regex_str` and search for it in `string`, replacing all matches with `replace`, and returning the new string.
+
+	`replace` can be a string or a function.
+
+	If it is a string then escape sequences of the form `\<number>` and `\g<number>` can be used to expand to the corresponding group (or an empty string for unmatched groups).
+
+	If `replace` is a function then it must take a single argument (the match) and should return a replacement string.
+
+	If `count` is specified and non-zero then substitution will stop after this many substitutions are made.
+
+	The `flags` argument is ignored.
+
+	Note: availability of this function depends on MicroPython port.
+	'''
---
+++ code_completion/select.py
@@ -0,0 +1,93 @@
+'''
+wait for events on a set of streams
+
+This module implements a subset of the corresponding CPython module, as described below.
+
+For more information, refer to the original CPython documentation: select.
+
+This module provides functions to efficiently wait for events on multiple streams (select streams which are ready for operations).
+
+[View Doc](https://docs.micropython.org/en/latest/library/select.html)
+'''
+# Constants
+POLLIN = ...
+POLLOUT = ...
+POLLERR = ...
+POLLHUP = ...
+
+
+class Poll(object):
+	# Methods
+	def register(self, obj, eventmask: int = POLLIN | POLLOUT):
+		'''
+		Register `stream` obj for polling.
+
+		`eventmask` is logical OR of:
+
+		- `select.POLLIN` - data available for reading
+		- `select.POLLOUT` - more data can be written
+
+		Note that flags like `select.POLLHUP` and `select.POLLERR` are not valid as input eventmask (these are unsolicited events which will be returned from `poll()` regardless of whether they are asked for).
+
+		This semantics is per POSIX.
+
+		`eventmask` defaults to `select.POLLIN | select.POLLOUT`.
+
+		It is OK to call this function multiple times for the same `obj`.
+
+		Successive calls will update `obj`’s eventmask to the value of `eventmask` (i.e. will behave as `modify()`).
+		'''
+
+	def unregister(self, obj):
+		'''Unregister obj from polling.'''
+
+	def modify(self, obj, eventmask: int):
+		'''
+		Modify the `eventmask` for `obj`.
+
+		If `obj` is not registered, `OSError` is raised with error of ENOENT.
+		'''
+
+	def poll(self, timeout: int | None = -1, /) -> tuple | list:
+		'''
+		Wait for at least one of the registered objects to become ready or have an exceptional condition, with optional timeout in milliseconds (if `timeout` arg is not specified or -1, there is no timeout).
+
+		Returns list of `(`obj`, `event`, …)` tuples.
+
+		There may be other elements in tuple, depending on a platform and version, so don’t assume that its size is 2.
+
+		The `event` element specifies which events happened with a stream and is a combination of `select.POLL*` constants described above.
+
+		Note that flags `select.POLLHUP` and `select.POLLERR` can be returned at any time (even if were not asked for), and must be acted on accordingly (the corresponding stream unregistered from poll and likely closed), because otherwise all further invocations of `poll()` may return immediately with these flags set for this stream again.
+
+		In case of timeout, an empty list is returned.
+		'''
+
+	def ipoll(self, timeout: int | None = -1, flags: int = 0, /):
+		'''
+		Like `poll.poll()`, but instead returns an iterator which yields a `callee-owned tuple`.
+
+		This function provides an efficient, allocation-free way to poll on streams.
+
+		If `flags` is 1, one-shot behaviour for events is employed: streams for which events happened will have their event masks automatically reset (equivalent to `poll.modify(obj, 0)`), so new events for such a stream won’t be processed until new mask is set with `poll.modify()`.
+
+		This behaviour is useful for asynchronous I/O schedulers.
+
+		Difference to CPython:
+
+			This function is a MicroPython extension.
+		'''
+
+
+# Functions
+def poll() -> Poll:
+	'''Create an instance of the Poll class.'''
+
+def select(rlist, wlist, xlist, timeout=None):
+	'''
+	Wait for activity on a set of objects.
+
+	This function is provided by some MicroPython ports for compatibility and is not efficient.
+
+	Usage of `Poll` is recommended instead.
+	'''
