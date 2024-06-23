eb3a4e0f7b15cd970ac5a9cffc6c2c26c977ceb5
Walkline Wang <walkline@163.com>
2024-06-23 15:30:30 +0800

    extension: 增加多语言，增加自动更新插件设置功能
    每次插件自动更新后，设置中的路径依旧指向旧版本，因此在
    每次启动后增加判断功能，确定路径是否一致，不一致的话则
    自动更新设置
    
    Signed-off-by: Walkline Wang <walkline@163.com>

--- src/extension.ts
+++ src/extension.ts
@@ -5,19 +5,30 @@ import * as vscode from 'vscode';
 
 let ext = vscode.extensions.getExtension("walklinewang.code-completion-for-micropython");
 
-const config_name = "python";
-const key_1 = "autoComplete.extraPaths";
-const key_2 = "analysis.extraPaths";
-const key_3 = "analysis.stubPath";
-const value = `${ext?.extensionPath}\\code_completion`;
+const config_python = "python";
+const config_python_key_1 = "autoComplete.extraPaths";
+const config_python_key_2 = "analysis.extraPaths";
+const config_python_key_3 = "analysis.stubPath";
+const config_python_value = `${ext?.extensionPath}\\code_completion`;
+
+const config_micropython = "micropython";
+const config_micropython_key = "codeCompletion.enabled";
 
 // https://blog.csdn.net/forward_huan/article/details/108084802
 function update_configuration(cfgName: string, key: string, value: any,
-	configurationTarget: vscode.ConfigurationTarget | boolean) {
+	configurationTarget: vscode.ConfigurationTarget | boolean, force: boolean = false) {
 	return new Promise((resolve, reject) => {
 		let cfg = vscode.workspace.getConfiguration(cfgName);
 
-		if (cfg.has(key)) {
+		if (force) {
+			cfg.update(key, value, configurationTarget)
+				.then(() => {
+					resolve(true);
+				})
+				.then(undefined, error => {
+					reject(error);
+				});
+		} else if (cfg.has(key)) {
 			cfg.update(key, value, configurationTarget)
 				.then(() => {
 					resolve(true);
@@ -30,45 +41,55 @@ function update_configuration(cfgName: string, key: string, value: any,
 }
 
 export function activate(context: vscode.ExtensionContext) {
+	if (vscode.workspace.getConfiguration(config_micropython)
+		.get(config_micropython_key) === true) {
+		if (vscode.workspace.getConfiguration(config_python)
+			.get(config_python_key_3) !== config_python_value) {
+			vscode.commands.executeCommand('extension.enable', () => {});
+		}
+	}
+
 	const enable = vscode.commands.registerCommand('extension.enable', () => {
-		update_configuration(config_name, key_1, [value], vscode.ConfigurationTarget.Workspace)
+		update_configuration(config_python, config_python_key_1, [config_python_value], vscode.ConfigurationTarget.Workspace)
 		.then((resolved) => {
 			if (resolved) {
-				update_configuration(config_name, key_2, [value], vscode.ConfigurationTarget.Workspace)
+				update_configuration(config_python, config_python_key_2, [config_python_value], vscode.ConfigurationTarget.Workspace)
 				.then((resolved) => {
 					if (resolved) {
-						update_configuration(config_name, key_3, value, vscode.ConfigurationTarget.Workspace)
+						update_configuration(config_python, config_python_key_3, config_python_value, vscode.ConfigurationTarget.Workspace)
 						.then(() => {
-							vscode.window.showInformationMessage('Update settings successed.');
+							update_configuration(config_micropython, config_micropython_key, true, vscode.ConfigurationTarget.Workspace, true);
+							vscode.window.showInformationMessage(vscode.l10n.t("Update Settings Successed"));
 						});
 					}
 				});
 			} else {
-				vscode.window.showErrorMessage(`Update settings failed: Configuration key '${config_name}' not found.`, 'OK');
+				vscode.window.showErrorMessage(vscode.l10n.t("Update settings failed: Configuration key {0} not found.", config_python), vscode.l10n.t("OK"));
 			}
 		}).catch((error) => {
-			vscode.window.showErrorMessage(`Update settings failed: ${error.message}`, 'OK');
+			vscode.window.showErrorMessage(vscode.l10n.t("Update settings failed: {0}", error.message), vscode.l10n.t("OK"));
 		});
 	});
 
 	const disable = vscode.commands.registerCommand('extension.disable', () => {
-		update_configuration(config_name, key_1, [], vscode.ConfigurationTarget.Workspace)
+		update_configuration(config_python, config_python_key_1, [], vscode.ConfigurationTarget.Workspace)
 		.then((resolved) => {
 			if (resolved) {
-				update_configuration(config_name, key_2, [], vscode.ConfigurationTarget.Workspace)
+				update_configuration(config_python, config_python_key_2, [], vscode.ConfigurationTarget.Workspace)
 				.then((resolved) => {
 					if (resolved) {
-						update_configuration(config_name, key_3, "", vscode.ConfigurationTarget.Workspace)
+						update_configuration(config_python, config_python_key_3, "", vscode.ConfigurationTarget.Workspace)
 						.then(() => {
-							vscode.window.showInformationMessage('Update settings successed.');
+							update_configuration(config_micropython, config_micropython_key, false, vscode.ConfigurationTarget.Workspace);
+							vscode.window.showInformationMessage(vscode.l10n.t("Update Settings Successed"));
 						});
 					}
 				});
 			} else {
-				vscode.window.showErrorMessage(`Update settings failed: Configuration '${config_name}' not found.`, 'OK');
+				vscode.window.showErrorMessage(vscode.l10n.t("Update settings failed: Configuration key {0} not found.", config_python), vscode.l10n.t("OK"));
 			}
 		}).catch((error) => {
-			vscode.window.showErrorMessage(`Update settings failed: ${error.message}`, 'OK');
+			vscode.window.showErrorMessage(vscode.l10n.t("Update settings failed: {0}", error.message), vscode.l10n.t("OK"));
 		});
 	});
 
