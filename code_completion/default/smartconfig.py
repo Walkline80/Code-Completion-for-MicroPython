608cc6c7d356589dd61b900dd24edaf01b0226b5
Walkline Wang <walkline@163.com>
2024-09-17 14:52:55 +0800

    更新说明文档
    Signed-off-by: Walkline Wang <walkline@163.com>

--- code_completion/default/smartconfig.py
+++ code_completion/default/smartconfig.py
@@ -13,6 +13,10 @@ TYPE_AIRKISS: int = ...
 TYPE_ESPTOUCH_AIRKISS: int = ...
 TYPE_ESPTOUCH_V2: int = ...
 
+
+def version():
+	'''获取 SmartConfig 内部版本号'''
+
 def start():
 	'''开启配网功能'''
 
@@ -26,13 +30,23 @@ def done() -> bool:
 	返回`True`表示已获取配网信息，返回`False`表示未获取到配网信息。
 	'''
 
+def timeout():
+	'''设置/获取配网超时时间，单位为秒，有效值范围：15~255'''
+
+def fast_mode():
+	'''设置/获取配网模式，默认为正常模式'''
+
+def v2_key():
+	'''设置/获取 EspTouch V2 协议使用的密钥，长度为16字节，传递`None`或空字符串清空秘钥'''
+
 def info() -> tuple:
 	'''
 	获取配网信息，返回值包含如下信息::
 
-	    ('ssid', 'password', b'bssid', type[, b'rvd_data'])
-
-	- rvd_data: EspTouch V2 custom data
+	```python
+		# v2_data - EspTouch V2 custom data
+	    ('ssid', 'password', b'bssid', type[, b'v2_data'])
+	```
 	'''
 
 def ssid() -> str:
@@ -52,5 +66,5 @@ def type() -> int:
 def type(value: int):
 	'''设置配网协议类型'''
 
-def rvd_data() -> bytes:
+def v2_data() -> bytes:
 	'''获取 EspTouch V2 自定义数据'''
